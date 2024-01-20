9dbffc74fcbc9674ba24d85e49c7483b14fabf55
Sarah <sarahpardeike@gmail.com>
2024-01-20 20:54:34 +0100

    added email model and form to html, html shows list of all emails

--- emailsender/models.py
+++ emailsender/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Email(models.Model):
+    emailadress = models.EmailField()
+
+    def __str__(self):
+        return self.emailadress
--- emailsender/views.py
+++ emailsender/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Email
 
 # Create your views here.
+
+def add_email(request):
+
+    email = request.POST.get('email-adress')
+    if email != None:
+        Email.objects.create(emailadress=email)
+
+    emaillist = Email.objects.all()
+
+    context = {
+        'email_list': emaillist,
+    }
+
+    return render(request, 'email.html', context)
