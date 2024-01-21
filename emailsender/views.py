b32931b3bcf89ee27bd93492ab00d77dd949e3f1
Sarah <sarahpardeike@gmail.com>
2024-01-21 14:25:23 +0100

    added mailsending functionality, added css

--- emailsender/views.py
+++ emailsender/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Email
 from psycopg2 import errors
 from django.db.utils import IntegrityError
+from django.core.mail import BadHeaderError, send_mass_mail
+from main.settings import EMAIL_HOST_USER
 
-# Create your views here.
+
 
 def add_email(request):
-
+    # Function to add an email address and return a list of all email objects
+    
     email = request.POST.get('email-adress')
     
     if email != None:
@@ -15,6 +18,23 @@ def add_email(request):
         except (errors.UniqueViolation, IntegrityError):
             return redirect('add-email')
 
+    subject = request.POST.get("subject")
+    message = request.POST.get("message")
+    to_email_list = request.POST.getlist("receiver_input")
+
+    datatuple = ()
+
+    if subject and message and to_email_list:
+        try:
+            for mail in to_email_list:
+                data = (subject, message, EMAIL_HOST_USER, [mail])
+                datatuple += (data,)
+            #print(datatuple)
+            send_mass_mail(datatuple)
+        except BadHeaderError:
+            return redirect('add-email')
+    
+    
     emaillist = Email.objects.all()
 
     context = {
@@ -22,3 +42,36 @@ def add_email(request):
     }
 
     return render(request, 'email.html', context)
+
+# def send_mail(request):
+#     # Function to send mails
+
+#     from_email = request.POST.get("email")
+#     subject = request.POST.get("subject")
+#     message = request.POST.get("message")
+#     to_email_list = request.POST.get("receiver_input")
+
+#     print('Hallo')
+#     print(from_email)
+#     print(subject)
+#     print(message)
+#     print(to_email_list)
+
+    
+#     if subject and message and from_email and to_email_list:
+#         try:
+#             datatuple = ()
+
+#             for mail in to_email_list:
+#                 datatuple += (subject, message, from_email, mail)
+
+#             print(datatuple)
+#             send_mass_mail(datatuple)
+#         except BadHeaderError:
+#             return redirect('add-email')
+#         return redirect('add-email')
+#     else:
+#         print('leeres doc')
+#         return redirect('add-email')
+
+    
--- main/settings.py
+++ main/settings.py
@@ -123,3 +123,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static/']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'app-password'
+EMAIL_USE_TLS = True
+
